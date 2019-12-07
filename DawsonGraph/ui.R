cbe2064b14fd685d7de5a5b24e931390b5e37865
zillaton <57115942+zillaton@users.noreply.github.com>
2019-12-06 16:13:34 -0800

    Add files via upload

---
+++ DawsonGraph/ui.R
@@ -0,0 +1,55 @@
+library(shiny)
+library(datasets)
+
+ui <- shinyUI(fluidPage(
+  titlePanel("Column Plot"),
+  tabsetPanel(
+    tabPanel("Upload File",
+             titlePanel("Uploading Files"),
+             sidebarLayout(
+               sidebarPanel(
+                 fileInput('file1', 'Choose CSV File',
+                           accept=c('text/csv', 
+                                    'text/comma-separated-values,text/plain', 
+                                    '.csv')),
+                 
+                 # added interface for uploading data from
+                 # http://shiny.rstudio.com/gallery/file-upload.html
+                 tags$br(),
+                 checkboxInput('header', 'Header', TRUE),
+                 radioButtons('sep', 'Separator',
+                              c(Comma=',',
+                                Semicolon=';',
+                                Tab='\t'),
+                              ','),
+                 radioButtons('quote', 'Quote',
+                              c(None='',
+                                'Double Quote'='"',
+                                'Single Quote'="'"),
+                              '"')
+                 
+               ),
+               mainPanel(
+                 tableOutput('contents')
+               )
+             )
+    ),
+    tabPanel("First Type",
+             pageWithSidebar(
+               headerPanel('My First Plot'),
+               sidebarPanel(
+                 
+                 # "Empty inputs" - they will be updated after the data is uploaded
+                 selectInput('xcol', 'X Variable', ""),
+                 selectInput('ycol', 'Y Variable', "", selected = "")
+                 
+               ),
+               mainPanel(
+                 plotOutput('MyPlot')
+               )
+             )
+    )
+    
+  )
+)
+)
